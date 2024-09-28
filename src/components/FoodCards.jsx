5a2bafe148b4a82dc7741ce6d17536c765c96c2a
unknown <luiszyzz248@gmail.com>
2024-09-28 15:32:51 -0300

    modificando tarjeta

--- src/components/FoodCards.jsx
+++ src/components/FoodCards.jsx
@@ -22,13 +22,13 @@ const FoodCards = (props) => {
           backgroundPosition: props.backgroundPosition || 'center' 
         }}
       >
-        <div className='card-front flex flex-col items-center justify-center rounded-lg'>
-          <div className='h-full w-full flex flex-col items-center justify-center bg-white bg-opacity-30 rounded-lg p-4'>
-            <h3 className='text-[30px] text-black font-bold mb-2'>{props.name}</h3>
+        <div className='card-front flex flex-col items-center justify-center rounded-lg border border-white'>
+          <div className='h-full w-full flex flex-col items-center justify-center bg-[#00000034] rounded-[50px] p-4 '>
+            <h3 id='textShadow' className='text-[40px] text-white font-extrabold mb-2 border-b'>{props.name}</h3>
             {/* Removed description and price from the front */}
           </div>
         </div>
-        <div className='card-back flex flex-col items-center gap-[10px] justify-center bg-white rounded-lg p-4'>
+        <div className='card-back flex flex-col items-center gap-[10px] justify-center bg-white rounded-[50px] p-4'>
           <img src={props.typeIcon} alt="Category Icon" className='w-[50px] text-[#0F1D15]' />
           <h3 className='text-[30px] text-[#0F1D15] font-bold'>{props.name}</h3>
           <p className='text-lg text text-center text-[#0F1D15]'>{props.description}</p> {/* Moved description here */}
@@ -43,6 +43,7 @@ const FoodCards = (props) => {
         </div>
       </div>
     </div>
+    
   );
 }
 
